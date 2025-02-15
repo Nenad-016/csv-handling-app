33fb95b99e945c166b61c5d93a0813360dec08c4
Nenad016 <nenad.jovanovic@paprikart.co.rs>
2025-02-15 13:05:37 +0100

    Routes created and initial controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'categories', 'middleware' => 'api'], function () {
+    Route::get('', [CategoryController::class, 'index']);
+    Route::put('/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'products', 'middleware' => 'api'], function () {
+    Route::get('', [ProductController::class, 'index']);
+    Route::put('/{id}', [ProductController::class, 'showByCategory']);
+    Route::delete('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
 });
